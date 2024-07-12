fe2cb17916ab2feea552d5f54c1a3903ec928529
classFrancis <121095986+classFrancis@users.noreply.github.com>
2024-07-11 22:38:40 -0400

    Images by url object js, need a database host 4 optimization

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { Flex, Layout } from 'antd';
+import { Flex, Layout, Card } from 'antd';
+import { imagesUrl } from "./ImagesUrl";
 
+let { Meta } = Card;
 let { Header, Content, Footer } = Layout;
 
 function MainComponent() {
@@ -16,8 +18,43 @@ function MainComponent() {
 
                 </Header>
                 <Content>
-                    <Flex justify="center">
-                        <div>hola</div>
+                    <Flex style={{marginTop: "50px"}} justify="center" gap="large">
+                        <Card
+                            hoverable
+                            style={{
+                            width: 240,
+                            }}
+                            cover={<img alt="manga" src={imagesUrl.berserk4}/>}
+                        >
+                            <Meta title="Berserk tomo 4"/>
+                        </Card>
+                        <Card
+                            hoverable
+                            style={{
+                            width: 240,
+                            }}
+                            cover={<img alt="manga" src={imagesUrl.berserk5}/>}
+                        >
+                            <Meta title="Berserk tomo 5"/>
+                        </Card>
+                        <Card
+                            hoverable
+                            style={{
+                            width: 240,
+                            }}
+                            cover={<img alt="manga" src={imagesUrl.berserk14}/>}
+                        >
+                            <Meta title="Berserk tomo 14"/>
+                        </Card>
+                        <Card
+                            hoverable
+                            style={{
+                            width: 240,
+                            }}
+                            cover={<img alt="manga" src={imagesUrl.berserk15}/>}
+                        >
+                            <Meta title="Berserk tomo 15"/>
+                        </Card>
                     </Flex>
                 </Content>
                 <Footer style={{
